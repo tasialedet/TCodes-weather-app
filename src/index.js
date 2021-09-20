b964793fcf25a25c6d6714ea3e50f38eb7e6cc23
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-09-20 08:32:00 -0500

    fixing NaN

--- src/index.js
+++ src/index.js
@@ -54,20 +54,20 @@ function displayForecast(response) {
   let precipMeasure = Math.round(response.data.daily[0].rain);
   precipElement = document.querySelector("#weather-precip");
 
-  function measurePrecip(x)
+  function measurePrecip(x) {
+    if (isNaN(x)) {
+      return 0;
+    }
+    let x = precipMeasure;
 
-  if (isNaN(x)) {
-    return 0;
+    if (precipMeasure > 0) {
+      precipElement.innerHTML = `${precipMeasure} %`;
+    }
+    if (precipMeasure === 0) {
+      precipElement.innerHTML = `0 %`;
+    }
   }
-  let x = precipMeasure;
-
-  if (precipMeasure > 0) {
-    precipElement.innerHTML = `${precipMeasure} %`;
-  }
-  if (precipMeasure === 0) {
-    precipElement.innerHTML = `0 %`;
-  }
-measurePrecip();
+  measurePrecip();
 
   let forecast = response.data.daily;
 
